07f5e9e4a406ef69bd230ee52961d3d7976228e2
redperezz <kaicruzz91@gmail.com>
2021-09-29 13:32:50 -0400

    Completed all but last problem for Strings, last problem needs more syntax lookup

--- 01Strings/String/Tester+Strings.swift
+++ 01Strings/String/Tester+Strings.swift
@@ -11,28 +11,34 @@ extension Tester {
 
     /// Using print command
     func printWord(word: String) {
-        
+        print(word)
     }
     
     /// input "hello" should print "HELLO"
     func printUpperCase(word: String) {
-        
+        print(word.uppercased())
     }
     
     /// Using for loop, print each char in word
     func printLetters(word: String) {
-        
+        for letter in word {
+            print(letter)
+        }
     }
     
     /// given a word, flip its characters, and print the result
     /// e.g. football -> llabtoof
     func flipWord(word: String) {
-        
+        print(word.reversed())
     }
     
     /// Using for-in-enumerated(), print odd-position chars in word
     func printOddCharsEnumerated(word: String) {
-        
+        for (idx, letter) in word.enumerated() {
+            if idx % 2 == 0 {
+                print(letter)
+            }
+        }
     }
     
     /**
@@ -44,11 +50,27 @@ extension Tester {
      one -> oneyay
      */
     func printPigLatin(word: String) {
-        
+        var word = word
+        let vowels: [String] = ["a", "e", "i", "o", "u"]
+        let firstLetter = String(word[word.startIndex])
+        if vowels.contains(firstLetter) {
+            word += "yay"
+        } else {
+            word.remove(at: word.startIndex)
+            word.insert(Character(firstLetter), at: word.endIndex)
+            word += "ay"
+        }
     }
     
     /// Using loop and string.index, return true if input is a palindrome
     func isPalindrome(word: String) -> Bool {
+//        var lastIdx = word.index(before: word.endIndex)
+//        for (_, letter) in word.enumerated() {
+//            if letter != word[word.index(before: word.endIndex)] {
+//                return false
+//            }
+////            lastIdx - 1
+//        }
         return false
     }
 }
