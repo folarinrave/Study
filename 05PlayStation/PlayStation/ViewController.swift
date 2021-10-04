5bfed32e05c6f3798d30ceae273c4d988f51b5f1
Ling Hong <bohrsmodel@gmail.com>
2021-10-04 09:55:39 -0400

    PlayStation assignment

---
+++ 05PlayStation/PlayStation/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  PlayStation
+//
+//  Created by rave on 10/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var PlayStation_View: PlayStation!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
