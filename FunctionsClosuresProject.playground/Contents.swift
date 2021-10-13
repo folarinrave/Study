e9e21b1a46192ffcb1460a95b83e6dd3ed8643fd
Folarin Williamson <>
2021-10-13 10:24:21 -0400

    test commit

---
+++ FunctionsClosuresProject.playground/Contents.swift
@@ -0,0 +1,70 @@
+import UIKit
+
+
+// PRACTICE 1
+
+// There are 32 NFL teams
+// Create a function that a has one parameter with a type Int
+// Give that parameter a default value of 32
+// in the body of the function, check if the value is 32
+// if the value is 32, print("hell yea")
+// if the value is not 32 print("hell no")
+// call the function you created once to print "hell yea"
+// call the function you crated another time to print "hell no"
+// ---------------------------------------------------------------------------------------
+
+// PRACTICE 2
+
+// create a (1)constant of type [Int] and set it equal to your favorite number (only 1 needed)
+// create a function that has one parameter with a type Int, and a return type String
+// inside your function, convert the Int to a new String type with the following message: "My favorite number is 32" and return it
+// create another (2)constant with a type [String] below the function you created
+// using `map`, set (2)constant = to the result of calling map over your array of favorite numbers
+//
+
+// ---------------------------------------------------------------------------------------
+
+// PRACTICE 3
+
+// create a function that takes a parameter with the following type: a closure with 1 String parameter type and no return value
+// on the next line call that function
+// "yes, it doesn't need to do anything"
+
+// ---------------------------------------------------------------------------------------
+
+// PRACTICE 4
+
+// create a function without any argument labels
+// this function should have 2 parameters of type Double and a return type of String
+// return a string that looks like the following: "4.0 * 2.0 = 8.0"
+// call this function and print the String to the console
+
+// ---------------------------------------------------------------------------------------
+
+// PRACTICE 5
+
+// create a function with 2 parameters of type String
+// this function should return 2 values
+// each value should be equal to the first
+
+// ---------------------------------------------------------------------------------------
+
+// PRACTICE 6
+
+// ---------------------------------------------------------------------------------------
+
+// PRACTICE 7
+
+// ---------------------------------------------------------------------------------------
+
+// PRACTICE 8
+
+// ---------------------------------------------------------------------------------------
+
+// PRACTICE 9
+
+// ---------------------------------------------------------------------------------------
+
+// PRACTICE 10
+
+
