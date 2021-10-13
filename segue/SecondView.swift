73b8d8fe02fbcb9bc9e7de7b7e27b2544962367b
Nathaniel Patterson <odst0016@gmail.com>
2021-10-13 07:33:24 -0400

    having issues with delegation 'working with prepare' still going to try and get delegation working

---
+++ segue/SecondView.swift
@@ -0,0 +1,33 @@
+//
+//  SecondView.swift
+//  segue
+//
+//  Created by Nathaniel Patterson on 10/13/21.
+//
+
+import UIKit
+
+class SecondView: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        names.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = names[indexPath.row]
+        return cell
+    }
+    
+    var names: [String] = []
+    var name: String = ""
+    @IBOutlet weak var table: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        table.delegate = self
+        table.dataSource = self
+        names.append(name)
+    }
+
+
+}
--- segue/ViewController.swift
+++ segue/ViewController.swift
@@ -7,13 +7,26 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBAction func toSecondView(_ sender: Any) {
+        performSegue(withIdentifier: "toSecondView", sender: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toSecondView" {
+            print("Its Working")
+            let destinationVC = segue.destination as! SecondView
+            destinationVC.name = textField.text ?? ""
+        }
+    }
 
 }
 
